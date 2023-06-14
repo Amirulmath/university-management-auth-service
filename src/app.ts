d4edbb1b6e3573998b3b9efa97803f22f7e3db94
Amirulmath <amirulmath760@gmail.com>
2023-06-14 20:14:19 +0600

    Setup ESlint, pretter, husky, lint-stage

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app : Application = express()
-const port = 3000
+const app: Application = express()
 
 app.use(cors())
 
-app.get('/', (req:Request, res:Response) => {
+//parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Working Successfully')
-});
+})
 
 export default app
