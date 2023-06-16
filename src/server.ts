1f62a283b41821d5ff3fb94dc27649ba902ca592
Amirulmath <amirulmath760@gmail.com>
2023-06-16 21:01:36 +0600

    Create user service

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ async function boostrap() {
     console.log(`🛢   Database is connected successfully`)
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
+      console.log(`Application  listening on port ${config.port}`)
     })
   } catch (err) {
     console.log('Failed to connect database', err)
